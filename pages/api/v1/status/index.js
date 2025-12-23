7387e8ef740e3b3dd9ed114dd3ad8980b4cbc409
Kleison Vitt <kleison.vittm@outlook.com>
2025-12-23 10:16:42 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,11 @@
 // /api/status
 // método send não define o charset mas o json sim
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
+
   response.status(200).json({ Chave: "Valor" });
 }
 
