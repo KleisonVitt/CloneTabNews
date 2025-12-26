7a4b2d93cd1fd8a284422a7966509530e80a4d10
Kleison Vitt <kleison.vittm@outlook.com>
2025-12-26 13:40:40 -0300

    implementação aula 20

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,32 @@
 // /api/status
-// método send não define o charset mas o json sim
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ Chave: "Valor" });
+  const postgresVersion = (await database.query("SELECT version();")).rows[0]
+    .version;
+
+  const maxConnections = (
+    await database.query(
+      "SELECT setting::int AS max_connections FROM pg_settings WHERE name = 'max_connections';",
+    )
+  ).rows[0].max_connections;
+
+  const dbConnections = (
+    await database.query("SELECT count(*)::int FROM pg_stat_activity;")
+  ).rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: maxConnections,
+        connections: dbConnections,
+      },
+    },
+  });
 }
 
 export default status;
