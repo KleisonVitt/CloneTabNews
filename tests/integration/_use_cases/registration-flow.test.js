2da56ff8c39305215099f3dcb10ab0c3a7613120
Kleison Vitt <kleison.vittm@outlook.com>
2026-03-14 12:17:55 -0300

    feat: require `read:session` to access `/user` endpoint

--- tests/integration/_use_cases/registration-flow.test.js
+++ tests/integration/_use_cases/registration-flow.test.js
@@ -80,7 +80,7 @@ describe("Use case: Registration FLow (all successful)", () => {
     expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
 
     const activatedUser = await user.findOneByUsername("RegistrationFlow");
-    expect(activatedUser.features).toEqual(["create:session"]);
+    expect(activatedUser.features).toEqual(["create:session", "read:session"]);
   });
 
   test("Login", async () => {
@@ -104,5 +104,16 @@ describe("Use case: Registration FLow (all successful)", () => {
     expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
   });
 
-  test("Get user information", async () => {});
+  test("Get user information", async () => {
+    const userResponse = await fetch("http://localhost:3000/api/v1/user", {
+      headers: {
+        Cookie: `session_id=${createSessionsResponseBody.token}`,
+      },
+    });
+    expect(userResponse.status).toBe(200);
+
+    const userResponseBody = await userResponse.json();
+
+    expect(userResponseBody.id).toBe(createUserResponseBody.id);
+  });
 });
