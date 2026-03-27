d1e795bc14fd7fbd2f705f69ac367737853445ec
Kleison Vitt <kleison.vittm@outlook.com>
2026-03-27 15:52:34 -0300

    refactor: explicitly return in /api/v1/status GET handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -49,5 +49,5 @@ async function getHandler(request, response) {
     databaseStatusObject,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
